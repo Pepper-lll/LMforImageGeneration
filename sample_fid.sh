54523509870eb3b000d9c0ba185f2051ad93d3c6
Xuantong LIU <58336971+Pepper-lll@users.noreply.github.com>
2024-11-10 11:51:44 +0800

    Add files via upload

--- sample_fid.sh
+++ sample_fid.sh
@@ -9,4 +9,4 @@ TORCH_DISTRIBUTED_DEBUG=INFO torchrun --nnodes=1 --nproc_per_node=8 sample_fid.p
  --latent_shape 1 16 16 --deterministic --top-k 1000 --gen_iter_num 256 --temperature 1.0 \
  --num_images 32000 --per_proc_batch_size 4 --global_seed 64
 
-python -m pytorch_fid /app/ms/AIGC/haoshaozhe/fid_test/pytorch_fid/imagenet/train.npz images_fid_ar/L-2-10-g256-linearcfg-4.0-t1.0-g256-top1000 --num-workers 4
+python -m pytorch_fid /your/path/to/referred/imagenet/train.npz your/path/to/generated/images --num-workers 4
